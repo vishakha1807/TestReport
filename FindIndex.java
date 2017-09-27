ddf91c065ecfc05a89331abf23f3729a6f8b11ce
vishakha1807 <vishakha1807@gmail.com>
2017-09-27 23:26:02 +0530

    Update FindIndex.java

--- FindIndex.java
+++ FindIndex.java
@@ -1,10 +1,12 @@
 //1. Given a String Vishak1a, find out the index of 1 in lowest complexity.
-
+import java.util.Scanner;  
 public class FindIndex {
 	
-	public static void main(String[] args) {
-		String str = "Vishak1a";
-		System.out.println(str.split("[a-z]").length+1);
+	public static void main(String[] args) { 
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a string with a interger i.e. vishak1a");  
+   		String str=sc.next();  				
+		System.out.println(str.split("[a-z]").length-1);
 		System.out.println("Complexity is O(n)");
 
 		
